ed683bcf0ebf9a1016aeca5eb2b082bab4c3b776
QuangAnh845 <quanganh845@gmail.com>
2025-03-02 21:43:19 +0700

    add Lab01

--- Lab01/Code/Helloworld.java
+++ Lab01/Code/Helloworld.java
@@ -1,6 +1,6 @@
 public class Helloworld {
     public static void main(String[] args) {
-        System.out.println("20235724 - Nguyen Dai Viet Hoang");
+        System.out.println("20235656 - Truong quang Anh");
         System.out.println("Xin chao cac ban!");
         System.out.println("Hello world!");
     }
