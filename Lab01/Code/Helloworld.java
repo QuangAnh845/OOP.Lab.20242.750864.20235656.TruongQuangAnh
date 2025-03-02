274673cbebbc74c17f5f1be87a9fe3cbeaa25804
QuangAnh845 <quanganh845@gmail.com>
2025-03-02 20:56:51 +0700

    add Lab01

---
+++ Lab01/Code/Helloworld.java
@@ -0,0 +1,7 @@
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("20235724 - Nguyen Dai Viet Hoang");
+        System.out.println("Xin chao cac ban!");
+        System.out.println("Hello world!");
+    }
+}
